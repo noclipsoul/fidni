6334a54bb99f0e10b169ac2f8c2c0fe0fc526bad
noclipsoul <youssefabm@outlook.fr>
2024-08-06 02:01:20 +0200

    fix 3 ?

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,34 +1,26 @@
 const { Pool } = require("pg");
 
-module.exports=({env})=>({
-
-
-connection:{
+module.exports = ({ env }) => ({
+  connection: {
     client: 'postgres',
     connection: {
-        host: env('DATABASE_HOST','127.0.0.1'),
-        port: env.int('DATABASE_PORT',''),
-        database: env('DATABASE_NAME',''),
-        user: env('DATABASE_USERNAME',''),
-        password: env('DATABASE_PASSWORD',''),
-        schema: env('DATABASE_SCHEMA',''),
-        ssl: env.bool('DATABASE_SSL',false)
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', ''),
+      user: env('DATABASE_USERNAME', ''),
+      password: env('DATABASE_PASSWORD', ''),
+      schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
     debug: false,
-    
-    pool:{
-        min: 0,
-        max: 10,
-        acquireTimeoutMillis: 600000,
-        createTimeoutMillis: 30000,
-        idleTimeoutMillis: 20000,
-        reapIntervalMillis: 20000,
-        createRetryIntervalMillis: 200
+    pool: {
+      min: 0,
+      max: 10,
+      acquireTimeoutMillis: 600000,
+      createTimeoutMillis: 30000,
+      idleTimeoutMillis: 20000,
+      reapIntervalMillis: 20000,
+      createRetryIntervalMillis: 200,
     },
-
-   
-},
-
-
-
+  },
 });
