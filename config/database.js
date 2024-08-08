cf69459fe33e632d19474bf6fbc5bd71836e0c30
noclipsoul <youssefabm@outlook.fr>
2024-08-08 03:00:39 +0200

    final ?

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@
       connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST','localhost'),
+          host: env('DATABASE_HOST'),
           port: env.int('DATABASE_PORT', 5433),
-          database: env('DATABASE_NAME','fidnidb'),
-          user: env('DATABASE_USERNAME','postgres'),
-          password: env('DATABASE_PASSWORD','fidni@ibsar123'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not required
         
         },
