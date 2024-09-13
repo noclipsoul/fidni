ce6b644d5f92b9253324d09d74ad5b49b43fc04a
noclipsoul <youssefabm@outlook.fr>
2024-09-13 05:43:33 +0200

    clean?

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@
         client: 'postgres',
         connection: {
           host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT', 5433),
+          port: env.int('DATABASE_PORT', 5432),
           database: env('DATABASE_NAME'),
           user: env('DATABASE_USERNAME'),
           password: env('DATABASE_PASSWORD'),
