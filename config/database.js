1077bb85c6f3182018c8a174b37db99ae7758613
noclipsoul <youssefabm@outlook.fr>
2024-09-13 05:51:32 +0200

    works ?

--- config/database.js
+++ config/database.js
@@ -1,16 +1,13 @@
-    // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        
-        },
-        debug: false,
-      },
-    });
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'fidnidb'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'yourpassword'),
+      ssl: false,
+    },
+  },
+});
