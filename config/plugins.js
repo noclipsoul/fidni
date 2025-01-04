6d9b9de8652278bcd6efc70bc73c5cb85688b8c1
noclipsoul <youssefabm@outlook.fr>
2025-01-04 22:36:32 +0100

    backworks fine base

--- config/plugins.js
+++ config/plugins.js
@@ -1,34 +1,34 @@
-module.exports = () => ({
-
-
-    seo: {
-        enabled: true,
-      },
-      meilisearch: {
-        config: {
-          // Your meili host
-          host: "http://51.195.222.168:7700",
-          // Your master key or private key
-          apiKey: "lkw-a4zF4d7CbpyR8miFcmLLUCX8rx-vxP4dbfj9vE4",
-        }
-      },
-      email: {
-        config: {
-          provider: 'nodemailer',
-          providerOptions: {
-            host: 'mail.fidni.tn', // Your Poste.io SMTP server
-            port: 587, // Use TLS port
-            secure: false, // Set to true if using port 465
-            auth: {
-              user: 'support@fidni.tn', // Email address
-              pass: 'QKG6HwXGHN', // Email password
-            },
-          },
-          settings: {
-            defaultFrom: 'support@fidni.tn',
-            defaultReplyTo: 'support@fidni.tn',
-          },
+module.exports = ({ env }) => ({
+  seo: {
+    enabled: true,
+  },
+  meilisearch: {
+    config: {
+      host: "http://51.195.222.168:7700",
+      apiKey: "lkw-a4zF4d7CbpyR8miFcmLLUCX8rx-vxP4dbfj9vE4",
+    },
+  },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'mail.fidni.tn',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'support@fidni.tn',
+          pass: 'QKG6HwXGHN',
         },
       },
-     
+      settings: {
+        defaultFrom: 'support@fidni.tn',
+        defaultReplyTo: 'support@fidni.tn',
+      },
+    },
+  },
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET','a2Wy1zVNcl2x2tSYc9Txfg=='),
+    },
+  },
 });
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['m3YaXhQAOg/T4f13GYKyvQ==','cGEu12nWbpVGDQXm53hFZQ==','wHuzHOnLb6RzOZuPJZJdMA==','XeKGnnfoHePC1lx3ahJa6A==']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
